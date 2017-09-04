e5c1d07baab00fda2f06b31f70f3f8b390fcb8f6
paul godfrey <paul.b.godfrey@gmail.com>
2017-09-04 02:12:53 +0000

    working on sensor exports for training data

--- ros_image_saver.py
+++ ros_image_saver.py
@@ -1,17 +1,10 @@
 #! /usr/bin/python
-# Copyright (c) 2015, Rethink Robotics, Inc.
-
-# Using this CvBridge Tutorial for converting
-# ROS images to OpenCV2 images
-# http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
-
-# Using this OpenCV2 tutorial for saving Images:
-# http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
 
 # rospy for the subscriber
 import rospy
 # ROS Image message
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import Joy
 # ROS Image message -> OpenCV2 image converter
 from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
@@ -20,27 +13,57 @@ import cv2
 # Instantiate CvBridge
 bridge = CvBridge()
 frame = 0
+throttle = 0
+steering = 0
 
 def image_callback(msg):
     global frame
-    print("Received an image!")
+
+    current_steering = steering
+    current_throttle = throttle
+
     try:
         # Convert your ROS Image message to OpenCV2
+	font = cv2.FONT_HERSHEY_SIMPLEX
         cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+	cv2_img = cv2.putText(cv2_img, str(current_steering), (20, 40), font, 1, (255,255,255), 2)
+	cv2_img = cv2.putText(cv2_img, str(current_throttle), (20, 80), font, 1, (255,255,255), 2)
     except CvBridgeError, e:
         print(e)
     else:
         # Save your OpenCV2 image as a jpeg 
         frame += 1
-        filename = "cam" + str(frame) + ".jpeg"
-        cv2.imwrite(filename, cv2_img)
+        filename = "./images/cam" + str(frame) + ".jpeg"
+        
+	output = filename + "," + str(current_throttle) + "," + str(current_steering)
+
+        with open('recording.csv', 'a') as recording:
+            recording.write(output)
+            recording.write("\n")
+
+	cv2.imwrite(filename, cv2_img)
+
+
+
+        print(output)
+
+
+def joystick_callback(msg):
+    global throttle, steering
+
+    throttle = msg.axes[3]
+    steering = msg.axes[2]
 
 def main():
     rospy.init_node('image_listener')
-    # Define your image topic
+
     image_topic = "rgb/image_rect_color"
+    joystick_topic = "vesc/joy"    
+
     # Set up your subscriber and define its callback
     rospy.Subscriber(image_topic, Image, image_callback)
+    rospy.Subscriber(joystick_topic, Joy, joystick_callback)
+
     # Spin until ctrl + c
     rospy.spin()
 
