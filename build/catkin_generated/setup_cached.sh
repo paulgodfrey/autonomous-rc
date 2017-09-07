e53cf8ee48803525f802bc2a6341d8afde3d92fc
paul godfrey <paul.b.godfrey@gmail.com>
2017-09-07 01:50:47 +0000

    adding rplidar ros packages and creating python tool for 2d range viz

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/nvidia/autonomous-rc/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/nvidia/autonomous-rc/devel/lib:/opt/ros/kinetic/lib:/usr/local/lib:/usr/local/cuda-8.0/lib64"
+export PKG_CONFIG_PATH="/home/nvidia/autonomous-rc/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/nvidia/autonomous-rc/build"
+export PYTHONPATH="/home/nvidia/autonomous-rc/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nvidia/autonomous-rc/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/nvidia/autonomous-rc/src:$ROS_PACKAGE_PATH"
